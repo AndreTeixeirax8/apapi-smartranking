2ac83bf4d3328a24dea6ec5f5188a1dec01f3408
andre.teixeira <andre-x8@hotmail.com>
2022-11-20 16:29:04 -0300

    Iniciado modulo de categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [JogadoresModule,MongooseModule.
-    forRoot('mongodb://localhost/smart',{useNewUrlParser: true, useUnifiedTopology: true})],
+    forRoot('mongodb://localhost/smart',{useNewUrlParser: true, useUnifiedTopology: true}), CategoriasModule],
   controllers: [],
   providers: [],
 })
