8db8e27beaa5639c648a4070caaa53fd1c4f5a03
andre.teixeira <andre-x8@hotmail.com>
2022-12-04 13:20:43 -0300

    Adicionado o module de desafios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
   imports: [JogadoresModule,MongooseModule.
-    forRoot('mongodb://localhost/smart',{useNewUrlParser: true, useUnifiedTopology: true}), CategoriasModule],
+    forRoot('mongodb://localhost/smart',{useNewUrlParser: true, useUnifiedTopology: true}), CategoriasModule, DesafiosModule],
   controllers: [],
   providers: [],
 })
